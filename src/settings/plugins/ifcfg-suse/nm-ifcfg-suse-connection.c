489cee10564f0e07f5d8bcf47991d8a131dd27fd
Bin Li <libin.charles@gmail.com>
2011-10-18 18:36:06 +0800

    let the suse support reading ifcfg file

---
+++ src/settings/plugins/ifcfg-suse/nm-ifcfg-suse-connection.c
@@ -0,0 +1,200 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* NetworkManager system settings service
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2011 SUSE.
+ */
+
+#include "nm-ifcfg-suse-connection.h"
+
+G_DEFINE_TYPE (NMIfcfgSUSEConnection, nm_ifcfg_suse_connection, NM_TYPE_SETTINGS_CONNECTION)
+
+#define NM_IFCFG_SUSE_CONNECTION_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), NM_TYPE_IFCFG_SUSE_CONNECTION, NMIfcfgSUSEConnectionPrivate))
+
+typedef struct {
+	gulong ih_event_id;
+
+	char *path;
+	int file_wd;
+
+	char *keyfile;
+	int keyfile_wd;
+
+	char *routefile;
+	int routefile_wd;
+
+	char *route6file;
+	int route6file_wd;
+
+	char *unmanaged;
+} NMIfcfgSUSEConnectionPrivate;
+
+NMIfcfgSUSEConnection *
+nm_ifcfg_suse_connection_new (const char *full_path,
+                              NMConnection *source,
+                              GError **error,
+                              gboolean *ignore_error)
+{
+	GObject *object;
+	NMIfcfgSUSEConnectionPrivate *priv;
+	NMConnection *tmp;
+	char *unmanaged = NULL;
+
+	g_return_val_if_fail (full_path != NULL, NULL);
+
+	/* If we're given a connection already, prefer that instead of re-reading */
+	if (source)
+		tmp = g_object_ref (source);
+	else {
+		tmp = connection_from_file (full_path, NULL, NULL, NULL,
+		                            &unmanaged,
+		                            &keyfile,
+		                            &routefile,
+		                            &route6file,
+		                            error,
+		                            ignore_error);
+		if (!tmp)
+			return NULL;
+	}
+
+	object = (GObject *) g_object_new (NM_TYPE_IFCFG_SUSE_CONNECTION,
+	                                   NM_IFCFG_SUSE_CONNECTION_UNMANAGED, unmanaged,
+	                                   NULL);
+	if (!object)
+		goto out;
+	return (NMIfcfgSUSEConnection *) object;
+}
+
+static void
+commit_changes (NMSettingsConnection *connection,
+                NMSettingsConnectionCommitFunc callback,
+                gpointer user_data)
+{
+}
+
+static void
+do_delete (NMSettingsConnection *connection,
+	       NMSettingsConnectionDeleteFunc callback,
+	       gpointer user_data)
+{
+	NMIfcfgSUSEConnectionPrivate *priv = NM_IFCFG_SUSE_CONNECTION_GET_PRIVATE (connection);
+
+	NM_SETTINGS_SUSE_CONNECTION_CLASS (nm_ifcfg_suse_connection_parent_class)->delete (connection, callback, user_data);
+}
+
+static void
+nm_ifcfg_suse_connection_init (NMIfcfgSUSEConnection *connection)
+{
+}
+
+static void
+finalize (GObject *object)
+{
+	NMIfcfgSUSEConnectionPrivate *priv = NM_IFCFG_SUSE_CONNECTION_GET_PRIVATE (object);
+	/*NMInotifyHelper *ih;
+
+	nm_connection_clear_secrets (NM_CONNECTION (object));
+
+	ih = nm_inotify_helper_get ();
+
+	if (priv->ih_event_id)
+		g_signal_handler_disconnect (ih, priv->ih_event_id);
+
+	g_free (priv->path);
+	if (priv->file_wd >= 0)
+		nm_inotify_helper_remove_watch (ih, priv->file_wd);
+
+	g_free (priv->keyfile);
+	if (priv->keyfile_wd >= 0)
+		nm_inotify_helper_remove_watch (ih, priv->keyfile_wd);
+
+	g_free (priv->routefile);
+	if (priv->routefile_wd >= 0)
+		nm_inotify_helper_remove_watch (ih, priv->routefile_wd);
+
+	g_free (priv->route6file);
+	if (priv->route6file_wd >= 0)
+		nm_inotify_helper_remove_watch (ih, priv->route6file_wd);
+*/
+	G_OBJECT_CLASS (nm_ifcfg_suse_connection_parent_class)->finalize (object);
+}
+
+static void
+set_property (GObject *object, guint prop_id,
+		    const GValue *value, GParamSpec *pspec)
+{
+	NMIfcfgSUSEConnectionPrivate *priv = NM_IFCFG_SUSE_CONNECTION_GET_PRIVATE (object);
+
+	switch (prop_id) {
+	case PROP_UNMANAGED:
+		priv->unmanaged = g_value_dup_string (value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+static void
+get_property (GObject *object, guint prop_id,
+		    GValue *value, GParamSpec *pspec)
+{
+	NMIfcfgSUSEConnectionPrivate *priv = NM_IFCFG_SUSE_CONNECTION_GET_PRIVATE (object);
+
+	switch (prop_id) {
+	case PROP_UNMANAGED:
+		g_value_set_string (value, priv->unmanaged);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+		break;
+	}
+}
+
+
+static void
+nm_ifcfg_suse_connection_class_init (NMIfcfgSUSEConnectionClass *ifcfg_suse_connection_class)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (ifcfg_suse_connection_class);
+	NMSettingsConnectionClass *settings_class = NM_SETTINGS_CONNECTION_CLASS (ifcfg_suse_connection_class);
+
+	g_type_class_add_private (ifcfg_suse_connection_class, sizeof (NMIfcfgSUSEConnectionPrivate));
+
+	/* Virtual methods */
+	object_class->set_property = set_property;
+	object_class->get_property = get_property;
+	object_class->finalize     = finalize;
+	settings_class->delete = do_delete;
+	settings_class->commit_changes = commit_changes;
+
+	/* Properties */
+	g_object_class_install_property
+		(object_class, PROP_UNMANAGED,
+		 g_param_spec_string (NM_IFCFG_SUSE_CONNECTION_UNMANAGED,
+						  "Unmanaged",
+						  "Unmanaged",
+						  NULL,
+						  G_PARAM_READWRITE));
+
+/* 	signals[IFCFG_CHANGED] =
+		g_signal_new ("ifcfg-changed",
+		              G_OBJECT_CLASS_TYPE (object_class),
+		              G_SIGNAL_RUN_LAST,
+		              0, NULL, NULL,
+		              g_cclosure_marshal_VOID__VOID,
+		              G_TYPE_NONE, 0);*/
+}
+
---
+++ src/settings/plugins/ifcfg-suse/nm-ifcfg-suse-connection.h
@@ -0,0 +1,63 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* NetworkManager system settings service
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright (C) 2011 SUSE.
+ */
+
+#ifndef NM_IFCFG_SUSE_CONNECTION_H
+#define NM_IFCFG_SUSE_CONNECTION_H
+
+G_BEGIN_DECLS
+
+#include <NetworkManager.h>
+#include <nm-settings-connection.h>
+
+#define NM_TYPE_IFCFG_SUSE_CONNECTION            (nm_ifcfg_suse_connection_get_type ())
+#define NM_IFCFG_SUSE_CONNECTION(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), NM_TYPE_IFCFG_SUSE_CONNECTION, NMIfcfgSUSEConnection))
+#define NM_IFCFG_SUSE_CONNECTION_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), NM_TYPE_IFCFG_SUSE_CONNECTION, NMIfcfgSUSEConnectionClass))
+#define NM_IS_IFCFG_SUSE_CONNECTION(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), NM_TYPE_IFCFG_SUSE_CONNECTION))
+#define NM_IS_IFCFG_SUSE_CONNECTION_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((obj), NM_TYPE_IFCFG_SUSE_CONNECTION))
+#define NM_IFCFG_SUSE_CONNECTION_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), NM_TYPE_IFCFG_SUSE_CONNECTION, NMIfcfgSUSEConnectionClass))
+
+#define NM_IFCFG_SUSE_CONNECTION_UNMANAGED "unmanaged"
+
+typedef struct {
+	NMSettingsConnection parent;
+} NMIfcfgSUSEConnection;
+
+typedef struct {
+	NMSettingsConnectionClass parent;
+} NMIfcfgSUSEConnectionClass;
+
+GType nm_ifcfg_suse_connection_get_type (void);
+
+NMIfcfgConnection *nm_ifcfg_suse_connection_new (const char *filename,
+                                                 NMConnection *source,
+                                                 GError **error,
+                                                 gboolean *ignore_error);
+
+const char *nm_ifcfg_suse_connection_get_path (NMIfcfgSUSEConnection *self);
+
+const char *nm_ifcfg_suse_connection_get_unmanaged_spec (NMIfcfgSUSEConnection *self);
+
+gboolean nm_ifcfg_suse_connection_update (NMIfcfgSUSEConnection *self,
+                                          GHashTable *new_settings,
+                                          GError **error);
+
+G_END_DECLS
+
+#endif /* NM_IFCFG_SUSE_CONNECTION_H */
